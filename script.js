26722be77b9de1ade1e7a3760e770c92b433b029
ManuelMannhold <m.mannhold@gmx.de>
2025-03-22 21:14:40 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,113 @@
+let offset = 0;
+let limit = 20;
+let pokemon = [];
+let pokemonData = [];
+let pokemonTypes = [];
+const typeColors = {
+  fire: "#F08030",
+  water: "#6890F0",
+  grass: "#78C850",
+  electric: "#F8D030",
+  ice: "#98D8D8",
+  fighting: "#C03028",
+  poison: "#A040A0",
+  ground: "#E0C068",
+  flying: "#A890F0",
+  psychic: "#F85888",
+  bug: "#A8B820",
+  rock: "#B8A038",
+  ghost: "#705898",
+  dragon: "#7038F8",
+  dark: "#705848",
+  steel: "#B8B8D0",
+  fairy: "#EE99AC",
+  normal: "#A8A878",
+};
+
+async function init() {
+  await fetchPokemon();
+  await fetchPokemonDetails();
+  displayPokemon();
+}
+
+async function fetchPokemon(offset, limit) {
+  let url = `https://pokeapi.co/api/v2/pokemon/?limit=${limit}&offset=${offset}`;
+  let response = await fetch(url);
+  let data = await response.json();
+
+  pokemon.push(...data.results);
+}
+
+async function fetchPokemonDetails() {
+  for (let j = 0; j < pokemon.length; j++) {
+    let url = pokemon[j].url;
+    let response = await fetch(url);
+    let dataPokemon = await response.json();
+
+    pokemonData.push(dataPokemon);
+    console.log(pokemonData);
+
+    getPokemonTypes(j);
+  }
+}
+
+function getPokemonTypes(i) {
+  pokemonTypes = pokemonData[i].types;
+  let pokeTypes = "";
+
+  if (pokemonTypes && pokemonTypes.length > 0) {
+    for (let j = 0; j < pokemonTypes.length; j++) {
+      const types = pokemonTypes[j].type.name;
+
+      pokeTypes += `
+                    <span class="show-pokemon-types ${types}">${types}</span>
+                `;
+    }
+  }
+  return pokeTypes;
+}
+
+function setPokemonCardBackground(types) {
+  let type1 = types[0];
+  let type2 = types[1] || types[0];
+
+  let color1 = typeColors[type1] || "#A8A878";
+  let color2 = typeColors[type2] || "#A8A878";
+
+  return `linear-gradient(135deg, ${color1} 50%, ${color2} 50%)`;
+}
+
+function getPokemonCries(i) {
+  let cries = pokemonData[i].cries.latest;
+  let audio = new Audio(cries);
+  return audio.play();
+}
+
+function displayPokemon() {
+  let content = document.getElementById("content");
+  content.innerHTML = "";
+
+  for (let i = 0; i < pokemon.length; i++) {
+    content.innerHTML += `
+            <div class="pokemon-card">
+              <header>
+              ${pokemonData[i].name}
+              #${pokemonData[i].id}
+              <img src="${
+                pokemonData[i].sprites.other.dream_world.front_default
+              }">
+              <span>
+                ${getPokemonTypes(i)}
+              </span>
+              <span><button onclick="getPokemonCries(${i})">Schrei</button></span>
+              </header>
+                <div class="pokemon-info">
+                  <span>
+                    Gewicht = ${pokemonData[i].weight / 10} Kg
+                    Größe = ${pokemonData[i].height * 10} cm
+                  </span>
+                </div>
+            </div>
+        `;
+  }
+}
